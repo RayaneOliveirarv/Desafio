cffd269479897c0ccf24a55f611f9b847c68eaf4
Rayane Oliveira <rayanecontato1@gmail.com>
2023-11-09 23:19:16 -0300

    Função listagem e busca

--- Api/ProdutosController.php
+++ Api/ProdutosController.php
@@ -26,6 +26,45 @@ class ProdutosController
         $stmt->execute();
 
         $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Contagem do total de produtos
+        $totalRegistros = $this->db->query("SELECT COUNT(*) FROM produtos")->fetchColumn();
+        $totalPaginas = ceil($totalRegistros / $registrosPorPagina);
+
+        //Array com as informações 
+        $resposta = [
+            'pagina_atual' => $pagina,
+            'total_paginas' => $totalPaginas,
+            'total_registros' => $totalRegistros,
+            'registros_por_pagina' => $registrosPorPagina,
+            'registros' => $produtos
+        ];
+
+        //retorno da resposta
+        return $response->withJson($resposta);
     }
+
+    public function buscarProduto(Request $request, Response $response, array $args) {
+        $id = $args['id'];
+
+        // Busca de produto pelo ID
+        $stmt = $this->db->prepare("SELECT * FROM produtos WHERE id = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $produto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //Verificação se o produto foi encontrado
+        if (!$produto) {
+            return $response->withJson(['mensagem' => 'Produto não encontrado'], 404);
+        }
+
+        return $response->withJson($produto);
+    }
+
+    public function criarProduto(Request $request, Response $response) {
+        $dados = $request->getParsedBody();
+
+        
 
 }
