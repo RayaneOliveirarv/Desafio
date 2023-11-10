5f03733f4d453724f3d73e111a295d7d1960d238
Rayane Oliveira <rayanecontato1@gmail.com>
2023-11-09 22:51:52 -0300

    cria funções de teste para cada método

---
+++ Testes/ProdutosTeste.php
@@ -0,0 +1,24 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ProdutosTeste extends TestCase {
+    public function testListagemProdutos() {
+        // Implemente o teste para a listagem de produtos
+    }
+
+    public function testCriarProduto() {
+        // Implemente o teste para a criação de produtos
+    }
+
+    public function testBuscarProduto() {
+        // Implemente o teste para a busca de um produto por ID
+    }
+
+    public function testAtualizarProduto() {
+        // Implemente o teste para a atualização de um produto
+    }
+
+    public function testExcluirProduto() {
+        // Implemente o teste para a exclusão de um produto
+    }
+}
